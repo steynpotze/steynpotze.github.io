97e10b7df61cb66a8a43549a05aeb683a6bfd298
Steyn Potze <steyn.potze@me.com>
2020-06-07 20:01:50 +0200

    Deploy: see https://github.com/steynpotze/steynpotze.github.io-source for changes.

---
+++ precache-manifest.903c0dd0078cdec12248b504e0771e6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4b2ba91e800f2174f3604c9ee9732b",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "6ba9d907bf63e26547fc",
+    "url": "/css/app.fdf13691.css"
+  },
+  {
+    "revision": "bdaee036dc62a59439619781d103fc20",
+    "url": "/img/me.bdaee036.jpeg"
+  },
+  {
+    "revision": "fee29a0cd7522662b8a83b837c5cee9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc95ea0ad00ecfd3d7fa",
+    "url": "/js/about.27370432.js"
+  },
+  {
+    "revision": "6ba9d907bf63e26547fc",
+    "url": "/js/app.a1c6176e.js"
+  },
+  {
+    "revision": "c2e2f8b795217f04f438",
+    "url": "/js/chunk-vendors.051ed444.js"
+  },
+  {
+    "revision": "6fd4e1f9bbcab072f47b2ce75a72da99",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
